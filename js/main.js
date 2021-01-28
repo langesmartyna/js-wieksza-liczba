7b9264c09f088b3a6a30c3e5d22879ad2b7730cb
langesmartyna <langesmartyna@users.noreply.github.com>
2021-01-28 13:58:06 +0100

    version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 7;
+let num2 = 5;
+
+function porownanie(number1, number2) {
+    if (number1 > number2 ) {
+        return number1;
+    } else if (number2 > number1) {
+        return number2;
+    }
+}
+
+console.log(porownanie(num1, num2)); 
+
